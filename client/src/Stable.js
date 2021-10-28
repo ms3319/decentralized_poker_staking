db5caf936d1fdc16a533f9b40a9dc4b0b08b07ea
ms3319 <ms3319@ic.ac.uk>
2021-10-28 15:16:10 +0100

    For some reason git was not adding Stable.js

---
+++ client/src/Stable.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Stable extends Component {
+    render(){
+        return(
+           <div className = "Stable">
+            
+                   
+                   
+                </div>             
+
+        );
+    }
+}
