bbfea2d44cc686c57c9b447b71c37d405065fd49
ms3319 <ms3319@ic.ac.uk>
2021-11-15 17:57:40 +0000

    [jd3219, ms3319] Implemented the signing of transactions and environment variables

--- oracle/main.js
+++ oracle/main.js
@@ -4,10 +4,34 @@ import Web3 from 'web3'
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const StakingContract = require('../client/src/contracts/Staking.json');
+import dotenv from 'dotenv';
+dotenv.config()
+console.log(process.env.BLOCKCHAIN_LINK)
+console.log(process.env.CONTRACT_ADDRESS)
+console.log(process.env.ACCOUNT_ADDRESS)
+console.log(process.env.PRIVATE_KEY)
 // TODO: These should be set with environment variables
-const web3 = new Web3("ws://127.0.0.1:7545")
-const contractAddress = '0x29ce3349226137f5266E739cA5131014064b4fBd'
+const web3 = new Web3(process.env.BLOCKCHAIN_LINK)
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const account = process.env.ACCOUNT_ADDRESS;
+const accountPrivateKey = process.env.PRIVATE_KEY;
+
 const contract = new web3.eth.Contract(StakingContract.abi, contractAddress)
+
+const signTransaction = async (id, profit) => {
+  const transaction = contract.methods.gamePlayed(id, profit) //.send({from: account}).then((err, result) => { if (err) {console.log(err)} else console.log(result) })
+  const options = {
+    to: contractAddress,
+    data: transaction.encodeABI(),
+    gas: await transaction.estimateGas({from: account}),
+    gasPrice: await web3.eth.getGasPrice() // or use some predefined value
+  };
+
+  const signed  = await web3.eth.accounts.signTransaction(options, accountPrivateKey);
+  const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction);
+
+  console.log(receipt)
+}
 
 const StakeStatus = {
   Requested: '0',
@@ -33,7 +57,7 @@ async function getFilledStakes() {
 
 function startListeners(watchedStakes) {
   startStakeListener(watchedStakes, contract)
-  startApiListener(watchedStakes, contract, 10000)
+  startApiListener(watchedStakes, contract, 10000, signTransaction)
 }
 
 getFilledStakes().then(stakes => startListeners(stakes))
