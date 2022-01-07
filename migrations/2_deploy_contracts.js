bc95cb511b615156867d0d391781d1fcf06a0eca
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2022-01-07 18:18:52 +0000

    Use standard token contract when deploying to ropsten

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,12 +4,15 @@ var StakeCoinFaucet = artifacts.require("StakeCoinFaucet");
 
 module.exports = async function(deployer, network, accounts) {
   // TODO: Only deploy if not already deployed
-  if (network !== "ropsten") {
+  let tokenContractAddress;
+  if (network === "ropsten") {
+    tokenContractAddress = "0xFDe1260d74D0E126d09a65037eF650bB7E320614"
+  } else {
     await deployer.deploy(StakeCoin, { from: accounts[0], overwrite: false });
+    tokenContractAddress = (await StakeCoin.deployed()).address;
   }
-  const stakeCoinInstance = await StakeCoin.deployed();
 
-  await deployer.deploy(StakeCoinFaucet, stakeCoinInstance.address);
+  await deployer.deploy(StakeCoinFaucet, tokenContractAddress);
   // TODO: Add option so that if the network is ropsten or etherum then we use the relevant Dai token address
-  await deployer.deploy(Staking, stakeCoinInstance.address);
+  await deployer.deploy(Staking, tokenContractAddress);
 };
