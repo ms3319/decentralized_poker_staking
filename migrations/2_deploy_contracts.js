a100ecccb213d85e4f4d7f21a78b6ab7734bcb6c
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2022-01-07 17:20:10 +0000

    Modify staking tests to pass

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,9 @@ var Staking = artifacts.require("Staking");
 var StakeCoin = artifacts.require("StakeCoin");
 var StakeCoinFaucet = artifacts.require("StakeCoinFaucet");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   // TODO: Only deploy if not already deployed
-  // await deployer.deploy(StakeCoin);
+  await deployer.deploy(StakeCoin, { from: accounts[0], overwrite: false });
   const stakeCoinInstance = await StakeCoin.deployed();
 
   await deployer.deploy(StakeCoinFaucet, stakeCoinInstance.address);
